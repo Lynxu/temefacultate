f549002aba63d8d97145f8676ee0648b08b3be74
serj99 <go_with_the_flow99@yahoo.com>
2017-01-19 15:25:37 +0200

    Updated.

--- prob_3_ev/main.c
+++ prob_3_ev/main.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void read(int a[40], int n, int x)
+void Read(int *length, int *start, int *w) //we get the addresses
 {
-    int i;
-    scanf("%i", &n);
-    for(i=0;i<n;i++)
-    {
-        printf("a[%i]=",i);
-        scanf("%i", &a[i]);
-        printf("Caracter citit!\n");
-    }
-    printf("\nx="); scanf("%i", &x);
-    printf("A fost citit x");
+    scanf("%i", &(*length)); // scanf will put value in the address of value sent as parameter
+                            // length it's a pointer, not an int
+
+    for(int i=0;i<(*length);i++)
+        scanf("%i", &(*(start++)));
+    scanf("%i", &(*w));
 }
 
-/*
-void write(float x)
+void Write(float x)
 {
-    FILE *out;
-    out=fopen("output.txt", "w+");
-    fprintf(out, "%f", x);
-    fclose(out);
+    FILE *out = fopen("output.txt", "w");
+    fprintf(out, "%f", x), fclose(out);
 }
-*/
+
 
 int control_digit(int x)
 {
@@ -43,60 +36,20 @@ int control_digit(int x)
 int main()
 {
 
-    int i, n, x, a[40];
+    int i, n, x, A[40];
     float S=0, nr=0;
 
-    read(a, *n, *x);
-
-    printf("%i", n);
-    /*
-    printf("%i", a[0]);
-    printf("%i", a[1]);
-    printf("%i", a[2]);
-    */
-
-    /*
-    scanf("%i", &n);
-    for(i=0;i<n;i++)
-    {
-        printf("a[%i]=",i);
-        scanf("%i", &a[i]);
-    }
-    printf("\nx="); scanf("%i", &x);
-
-    */
-
-   // printf("\n");
-
-
-/*
+    Read(&n, A, &x); //we send addresses
+                     //A it's the address of the first array element (A = &A[0] = &A)
+                     //Elements of the array are memorized into a continous block memory,
+                     //each address being one after the other
 
     for(i=0; i<n; i++)
-        printf("%d", a[i]);
+        if(control_digit(A[i]) == x)
+            S+=A[i], nr++;
 
+    Write(S/nr);
 
-*/
-
-
-
-  //  printf("\n");
-
-    /*
-    for(i=0; i<n; i++)
-    {
-        if(control_digit(a[i]) == x)
-        {
-            S+=a[i];
-            nr++;
-        }
-    }
-
-    printf("%f", S/nr);
-    */
-
-    //printf("%i", control_digit(917));
-
-//    write(S/nr);
     return 0;
 }
 
