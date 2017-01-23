d77b531d9246903d5175022d2990785c28ccbbf1
Lynxu <lynxuro@gmail.com>
2017-01-23 15:21:42 +0200

    Suma si produs n numere fisiere

---
+++ sum_prod_fisiere/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,s=0,p=1,x;
+    FILE *f;
+    scanf("%i",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%i",&x);
+        if(x>0)
+        {
+            p*=x;
+            s+=x;
+        }
+    }
+    f=fopen("calcule.txt","w+");
+    fprintf(f,"p=%i\n",p);
+    fprintf(f,"s=%i",s);
+    return 0;
+}
