ebde192466bf2c72ca102f61581afa4a58100e91
serj99 <go_with_the_flow99@yahoo.com>
2017-10-11 09:09:00 +0300

    Updated. Moved all stdin to Read function.

--- inszero/main.c
+++ inszero/main.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
 
-void Read(int *n, float a[100])
+void Read(int *n, float a[100], float *x)
 {
     int i;
-    printf("n=");
+    printf("Input your array!\nn=");
     scanf("%i", n);
     for(i=0; i<*n; i++)
     {
         printf("a[%i]=", i);
         scanf("%f", &a[i]);
     }
+    printf("After what number would you like to add a zero?\nx=");
+    scanf("%f", x);
 }
 
-void Insert(int *n, float a[100])
+void AddZero(int *n, float a[100], int pos)
 {
-    int i, mem;
-    float x;
-    printf("x=");
-    scanf("%f", &x);
+    int i;
 
-    for(i=0; i<*n; i++)
-        if(a[i]==x)
-            mem=i; //position of x inside the array
-
-    //move elements to right
-    for(i=*n; i>=mem+2; i--)
+    //move elements to the right
+    for(i=*n; i>=pos+2; i--)
         a[i]=a[i-1];
 
-    //insert the zero
-    a[mem+1]=0;
+    a[pos+1]=0;
+
+    //update the length of the array
     (*n)++;
+}
+
+void Insert(int *n, float a[100], float x)
+{
+    int i, end;
+
+    //n*2-1 iterations to be sure we parse all the array
+    //posible input: all array elements could be equal with the x input value
+    end=*n*2-1;
+
+    i=0;
+    while(i<end)
+    {
+        if(a[i]!=x)
+            i++;
+        else
+        {
+            //it will parse from i position to end of array
+            AddZero(n, a, i);
+
+            i=i+2;
+        }
+    }
 }
 
 void Print(int n, float a[100])
@@ -43,17 +62,10 @@ void Print(int n, float a[100])
 int main()
 {
     int n;
-    float a[100];
-    Read(&n, a);
-
-    printf("The input array: ");
-    Print(n, a);
-    printf("\n");
-
-    Insert(&n, a);
-
+    float x, a[100];
+    Read(&n, a, &x);
+    Insert(&n, a, x);
     printf("The new array: ");
     Print(n, a);
-
     return 0;
 }
