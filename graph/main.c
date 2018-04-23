ded790f09edf875e222bd96e95a533cff81b192c
serj99 <go_with_the_flow99@yahoo.com>
2018-04-23 19:17:13 +0300

    Two graphs reunion.

--- graph/main.c
+++ graph/main.c
@@ -1,309 +1,247 @@
-//immplementarea uniui graf cu lista de liste de adiacenta
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
-struct AdjListCell
+struct adjvertexlist
 {
-	int index_node;
-	struct AdjListCell* next;
+    int index_vertex;
+    struct adjvertexlist *next;
 };
-struct AdjList
-{
-	struct AdjListCell *head;
-};
-struct NodeListCell
-{
-	int index_node;
-	struct NodeListCell *next;
-	struct AdjList *list;
-};
-struct NodeList
-{
-	struct NodeListCell *head;
-};
-struct GraphRep
-{
-	int nV;//numarul de noduri
-	int nE;//numarul de muchii
-	struct NodeList *Glist;//lista nodurilor grafului
-};
-typedef struct GraphRep Graph;
 
-/*functia de creare a unei celule pentru un nod al
-grafului in lista nodurilor grafului*/
-struct NodeListCell* newNodeListCell(int index_node)
+struct vertexlist
 {
-	struct NodeListCell* newNode =
-			(struct NodeListCell*) malloc(sizeof(struct NodeListCell));
-	newNode->index_node = index_node;
-	newNode->next = NULL;
-	newNode->list=NULL;
-	return newNode;
-}
-/*functia de constructie a unui graf*/
-Graph* createGraph()
-{
-	Graph* g = (Graph*) malloc(sizeof(Graph));
-	g->nV = 0;
-	g->nE=0;
-	g->Glist= (struct NodeList*) malloc (sizeof(struct NodeList));
-	g->Glist->head=NULL;
-	return g;
-}
-/*functia de adaugare a unui nod in graf*/
-Graph* addNode(Graph* g, int v)
-{
-	g->nV++;
-	struct NodeListCell *newNode=newNodeListCell(v);
-	newNode->next=g->Glist->head;
-	newNode->list=NULL;
-	g->Glist->head=newNode;
-	return g;
-}
+    int index_vertex;
+    struct vertexlist *next;
+    struct adjvertexlist *list;
+};
 
-int NodeAdded(Graph* g, int v)
+
+struct graphrep
 {
-    struct NodeListCell *nlist;
-    nlist = g->Glist->head;
-    if(nlist)
-	while(nlist)
-	{
-	    if(nlist->index_node == v)
-		return 1;
-	    nlist = nlist -> next;
-	}
+    int nv;
+    int ne;
+    struct vertexlist *head;
+};
+
+typedef struct graphrep graph;
+
+int VertexAdded(graph *g, int v)
+{
+    struct vertexlist *iterat_vlist;
+    iterat_vlist = g->head;
+
+    while(iterat_vlist)
+    {
+        if(iterat_vlist->index_vertex == v)
+        {
+            printf("Vertex %i found inside graph\n", v);
+            return 1;
+
+        }
+        iterat_vlist = iterat_vlist->next;
+    }
+    
+    printf("Vertex %i not found inside graph\n", v);
     return 0;
 }
 
-
-/*functia de creare a unei celule in lista de adiacenta asociata
-nodului unui graf*/
-struct AdjListCell* newAdjListCell(int index_node)
+graph* AddVertex(graph *g, int v)
 {
-	struct AdjListCell* newCell =
-			(struct AdjListCell*) malloc(sizeof(struct AdjListCell));
-	newCell->index_node = index_node;
-	newCell->next = NULL;
-	return newCell;
-}
-
-Graph* addEdge(Graph* g, int v, int w)
-{
-    g->nE++;
-
-    //we start from the last added vertex
-    //nlist = address of last added vertex
-    struct NodeListCell* nlist=g->Glist->head;
-
-    //we loop each vertex data structure
-    //from last to the first
-    while (nlist)
-    {
-        //edge will be added, from v vertex to w vertex
-	if (nlist->index_node==v)
-	{
-            //allocate memory for the w node data structure
-            //w node it's part of v's vertex list of edges
-	    struct AdjListCell* newCell = newAdjListCell(w);
-	    newCell->next = (struct AdjListCell*)nlist->list;
-	    nlist->list = (struct AdjList*)newCell;
-
-	}
-	//edge will be added, from w vertex to v vertex
-	if (nlist->index_node==w)
-	{
-	    //we allocate memory for the v node data structure
-            //v node it's part of w's vertex list of edges
-	    struct AdjListCell* newCell = newAdjListCell(v);
-	    newCell->next = (struct AdjListCell*)nlist->list;
-	    nlist->list = (struct AdjList*)newCell;
-	}
-	nlist=nlist->next;
-    }
+    struct vertexlist *new_vcell;
+    new_vcell = malloc(sizeof(struct vertexlist));
+    new_vcell->next = g->head;
+    g->head = new_vcell;
+    new_vcell->index_vertex = v;
+    new_vcell->list = NULL;
+    printf("Added %i vertex on graph\n", v);
     return g;
 }
 
-void showGraph(Graph* g)
+graph* AddEdge(graph *g, int v1, int v2)
 {
-	struct NodeListCell* nlist=g->Glist->head;
-	while (nlist)
+    printf("\nAddEdge() started\n");
+    printf("Edge to add from %i to %i and from %i to %i\n", v1, v2, v2, v1);
+    struct adjvertexlist *new_adjvcell;
+    struct vertexlist *iterat_vlist;
+    iterat_vlist = g->head;
+    while(iterat_vlist)
     {
-        struct AdjListCell* plist;
-        plist = (struct AdjListCell*)nlist->list;
-		printf("Lista de adiacenta a nodului %d este:",nlist->index_node);
-		if(plist)
-		while (plist)
-		{
-			printf("-> %d",plist->index_node);
-			plist = plist->next;
-		}
-		else
+        if(iterat_vlist->index_vertex == v1)
         {
-            printf("-> *");
+            new_adjvcell = malloc(sizeof(struct adjvertexlist));
+            new_adjvcell->index_vertex = v2;
+            new_adjvcell->next = iterat_vlist->list;
+            iterat_vlist->list = new_adjvcell;
+            printf("Added edge from %i to %i\n", iterat_vlist->index_vertex, v2);
         }
-        printf("\n");
-        nlist=nlist->next;
-    }
-}
-
-//1 if there's edge in graph g from vertex1 to vertex2
-//0 otherwise
-int EdgeFound(Graph* g, int vertex1, int vertex2)
-{
-    struct NodeListCell* nlist;
-    struct AdjListCell* plist;
-    
-    //there is edge from one vertex to same vertex 
-    if(vertex1 == vertex2)
-        return 1;
-
-    nlist = g->Glist->head;
-    while(nlist)
-    {
-        if(nlist->index_node == vertex1)
+        if(iterat_vlist->index_vertex == v2)
         {
-            plist = (struct AdjListCell*)nlist->list;
-            if(plist)
-		while(plist)
-		{
-		    if(plist->index_node == vertex2)
-			return 1;
-		    plist = plist->next;
-		}
+            new_adjvcell = malloc(sizeof(struct adjvertexlist));
+            new_adjvcell->index_vertex = v1;
+            new_adjvcell->next = iterat_vlist->list;
+            iterat_vlist->list = new_adjvcell;
+            printf("Added edge from %i to %i\n", iterat_vlist->index_vertex, v1);
         }
-	nlist = nlist->next;
+        iterat_vlist = iterat_vlist->next;
     }
-    return 0;
+    printf("AddEdge() finished\n\n");
+    return g;
 }
 
-Graph* CopyGraph(Graph* s, Graph* d)
+graph* ReadGraph(graph *g, FILE *f)
 {
-    struct NodeListCell* nlist;
-
-
-    nlist = d->Glist->head;
-
-    //for each vertex
-    while(nlist)
+    int i, x, y;
+    fscanf(f, "%i %i", &g->nv, &g->ne);
+    printf("Number of vertices read: %i\n", g->nv);
+    printf("Number of edges read: %i\n", g->ne);
+    for(i=1; i<=g->nv; i++)
     {
-        struct AdjListCell* plist;
-        plist = (struct AdjListCell*)nlist->list;
-
-        //if vertex has edges
-        if(plist)
-	    while(plist)
-	    {
-                //if the source graph s doesn't contain the edge
-                if(!EdgeFound(s, nlist->index_node, plist->index_node))
-		    s = addEdge(s, nlist->index_node, plist->index_node);
-		plist = plist->next;
-	    }
-        nlist=nlist->next;
+        fscanf(f, "%i", &x);
+        g = AddVertex(g, x);
     }
-    printf("\n");
-    return s;
-}
-
-
-Graph* GraphsReun(Graph* graph1,
-                             Graph* graph2)
-{
-    int i;
-    Graph* newgraph;
-
-    newgraph = createGraph();
-
-    //we append the biggest graph and after we
-    //append the smallest
-    if(graph1->nV > graph2->nV)
+    for(i=1; i<=g->ne; i++)
     {
-        for(i=1; i<=graph1->nV; i++)
-            newgraph = addNode(newgraph, i);
-	newgraph = CopyGraph(newgraph, graph1);
-	newgraph = CopyGraph(newgraph, graph2);
-    }
-    else
-    {
-        for(i=1; i<=graph2->nV; i++)
-            newgraph = addNode(newgraph, i);
-	newgraph = CopyGraph(newgraph, graph2);
-	newgraph = CopyGraph(newgraph, graph1);
-    }
-    return newgraph;
-
-}
-
-
-Graph* readGraph(Graph* g, FILE *f)
-{
-    int i, n, m, x, y;
-    fscanf(f,"%d%d",&n,&m);
-    printf("Numarul de noduri este %d\n",n);
-    printf("Numarul de muchii este %d\n",m);
-    for(i=1;i<=m;i++)
-    {
-        fscanf(f,"%d%d",&x,&y);
-        if(!NodeAdded(g, x))
-            g = addNode(g, x);
-        if(!NodeAdded(g, y))
-            g = addNode(g, y);
-        printf("Muchia %d %d\n", x, y);
-        g=addEdge(g,x,y);
+        fscanf(f, "%i %i", &x, &y);
+        printf("Edge read: %i, %i\n", x, y);
+        g = AddEdge(g, x, y);
     }
     fclose(f);
     return g;
 }
 
-Graph* createComplementGraph(Graph* g, Graph *c)
+void ShowGraph(graph *g)
 {
-    struct NodeListCell *vlist, *vlist2;
-    vlist = g->Glist->head;
-
-    if(vlist)
-    //we copy each node from the provided graph 
-    //to the complement graph
-    while(vlist)
+    struct vertexlist *iterat_vlist;
+    struct adjvertexlist *iterat_adjvlist;
+    iterat_vlist = g->head;
+    while(iterat_vlist)
     {
-	c = addNode(c, vlist->index_node);
-	vlist = vlist->next;
+        printf("%i -> : ", iterat_vlist->index_vertex);
+     
+        iterat_adjvlist = iterat_vlist->list;
+        while(iterat_adjvlist)
+        {
+            printf("T%i ", iterat_adjvlist->index_vertex);
+            iterat_adjvlist = iterat_adjvlist->next;
+        }
+        printf("\n");
+     
+        iterat_vlist = iterat_vlist->next;
     }
-    else
+
+}
+
+int EdgeFound(graph *g, int v1, int v2)
+{
+    struct vertexlist *iterat_vlist;
+    struct adjvertexlist *iterat_adjvlist;
+    iterat_vlist = g->head;
+    while(iterat_vlist)
     {
-	printf("Error. The graph whose complement you would like to compute it's null!");
-	return NULL;
+        if(iterat_vlist->index_vertex == v1)
+        {
+            iterat_adjvlist = iterat_vlist->list;
+            while(iterat_adjvlist)
+            {
+                if(iterat_adjvlist->index_vertex == v2)
+                {
+                    printf("Edge from %i to %i exists\n", iterat_vlist->index_vertex, iterat_adjvlist->index_vertex);    
+                    return 1;
+                }
+                iterat_adjvlist = iterat_adjvlist->next;
+            }
+        }
+        iterat_vlist = iterat_vlist->next;
+    }
+    printf("Edge from %i to %i doesn\'t exist\n", v1, v2);    
+    return 0;
+}
+
+graph* MakeReunion(graph *r, graph *g1, graph *g2)
+{
+    printf("FFFFF");
+    struct vertexlist *iterat_vlist;
+    struct adjvertexlist *iterat_adjvlist;
+    
+    printf("First graph adding\n");
+    //add vertices of first graph
+    iterat_vlist = g1->head;
+    while(iterat_vlist)
+    {
+        if(!VertexAdded(r, iterat_vlist->index_vertex))
+            r = AddVertex(r, iterat_vlist->index_vertex);
+        iterat_vlist = iterat_vlist->next;
+     } 
+    
+    //add edges of first graph
+    iterat_vlist = g1->head;
+    while(iterat_vlist)
+    {
+        iterat_adjvlist = iterat_vlist->list;
+        while(iterat_adjvlist)
+        {
+            if(!EdgeFound(r, iterat_vlist->index_vertex,
+                          iterat_adjvlist->index_vertex))
+                r = AddEdge(r, iterat_vlist->index_vertex,
+                            iterat_adjvlist->index_vertex);
+            iterat_adjvlist = iterat_adjvlist->next;
+        }
+        iterat_vlist = iterat_vlist->next;
     }
     
-    vlist = g->Glist->head;
-    //for each vertex data structure
-    while(vlist)
+
+    printf("Second graph adding\n");
+    //add vertices of second graph
+    iterat_vlist = g2->head;
+    while(iterat_vlist)
     {
-        vlist2 = g->Glist->head;
-        while(vlist2) 
+        if(!VertexAdded(r, iterat_vlist->index_vertex))
+            r = AddVertex(r, iterat_vlist->index_vertex);
+        iterat_vlist = iterat_vlist->next;
+     } 
+    
+    //add edges of first graph
+    iterat_vlist = g2->head;
+    while(iterat_vlist)
+    {
+        iterat_adjvlist = iterat_vlist->list;
+        while(iterat_adjvlist)
         {
-	    if(vlist->index_node != vlist2->index_node)
-		if(!EdgeFound(g, vlist->index_node, vlist2->index_node) &&
-                   !EdgeFound(c, vlist->index_node, vlist2->index_node))
-		    c = addEdge(c, vlist->index_node, vlist2->index_node);
-            vlist2 = vlist2->next;
+            if(!EdgeFound(r, iterat_vlist->index_vertex,
+                          iterat_adjvlist->index_vertex))
+                r = AddEdge(r, iterat_vlist->index_vertex,
+                            iterat_adjvlist->index_vertex);
+            iterat_adjvlist = iterat_adjvlist->next;
         }
-        vlist = vlist->next;
-    } 
-    return c;
+        iterat_vlist = iterat_vlist->next;
+    }
+    return r;
 }
 
 int main()
 {
-    FILE *f;
-    Graph *my_graph, *complement_graph;
-
-    f = fopen("graph.in", "r");
-    my_graph = createGraph();
-    my_graph = readGraph(my_graph, f);
-    showGraph(my_graph);
+    graph *g1, *g2, *rg;
+    FILE *f1, *f2;
+    g1 = malloc(sizeof(graph));
+    g1->head = NULL;
+    g2 = malloc(sizeof(graph));
+    g2->head = NULL;
+    f1 = fopen("graph1.txt", "r");
+    f2 = fopen("graph2.txt", "r");
+    g1 = ReadGraph(g1, f1);
+    printf("Graful 1\n");
+    ShowGraph(g1);
     printf("\n");
-    complement_graph = createGraph();
-    complement_graph = createComplementGraph(my_graph, complement_graph);
-    showGraph(complement_graph);
+    g2 = ReadGraph(g2, f2);
+    printf("Graful 2\n");
+    ShowGraph(g2);
+    
+    
+    rg = malloc(sizeof(graph));
+    rg->head = NULL;
+    rg = MakeReunion(rg, g1, g2);
+    printf("Graful reuniune\n");
+    ShowGraph(rg);
+    
+
     return 0;
 }
-
